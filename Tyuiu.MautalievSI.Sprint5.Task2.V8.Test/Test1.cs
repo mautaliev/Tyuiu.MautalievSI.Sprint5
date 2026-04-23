403e3922f14169d511c58b327941a22b47cff96f
Mautaliev Saidamir <mautalievsaidamir@gmail.com>
2026-04-23 22:59:27 +0500

    ertert

---
+++ Tyuiu.MautalievSI.Sprint5.Task2.V8.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.MautalievSI.Sprint5.Task2.V8.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
