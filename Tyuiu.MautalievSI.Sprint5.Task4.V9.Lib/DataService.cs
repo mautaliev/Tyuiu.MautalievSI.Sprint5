c025e8cd9821471a5f2bc4eb6bf106845007fe73
Mautaliev Saidamir <mautalievsaidamir@gmail.com>
2026-04-23 23:16:22 +0500

    Task4

---
+++ Tyuiu.MautalievSI.Sprint5.Task4.V9.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.MautalievSI.Sprint5.Task4.V9.Lib
+{
+    public class DataService : ISprint5Task4V9
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string X = File.ReadAllText(path);
+            double strX = double.Parse(X, CultureInfo.InvariantCulture);
+            double res = Math.Round((1 / Math.Sin(Convert.ToDouble(strX)) + Convert.ToDouble(strX) * Convert.ToDouble(strX)), 3);
+            return res;
+        }
+    }
+}
