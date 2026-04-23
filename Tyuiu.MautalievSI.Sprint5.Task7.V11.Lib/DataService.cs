91d7782cbd9b6855140f18a297cfa659539cf4f0
Mautaliev Saidamir <mautalievsaidamir@gmail.com>
2026-04-23 23:39:47 +0500

    Task7

---
+++ Tyuiu.MautalievSI.Sprint5.Task7.V11.Lib/DataService.cs
@@ -0,0 +1,42 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.MautalievSI.Sprint5.Task7.V11.Lib
+{
+    public class DataService : ISprint5Task7V11
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string pathSaveFile = @"C:\DataSprint5\OutPutDataFileTask7V10.txt";
+            FileInfo fileInfo = new FileInfo(pathSaveFile);
+            bool fileExists = fileInfo.Exists;
+            if (fileExists)
+            {
+                File.Delete(pathSaveFile);
+            }
+
+            string strLine = "";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        if ((char.IsUpper(line[i])) && (line[i] >= 'B') && (line[i] <= 'Z'))
+                        {
+                            strLine = strLine + line[i].ToString().ToLower();
+                        }
+                        else
+                        {
+                            strLine = strLine + line[i];
+                        }
+                    }
+
+                    File.AppendAllText(pathSaveFile, strLine + Environment.NewLine);
+                    strLine = "";
+                }
+            }
+            return pathSaveFile;
+        }
+    }
+}
---
+++ Tyuiu.MautalievSI.Sprint5.Task7.V11.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.MautalievSI.Sprint5.Task7.V11.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
