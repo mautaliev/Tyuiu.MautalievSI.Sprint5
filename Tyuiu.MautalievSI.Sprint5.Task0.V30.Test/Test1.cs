ad40af01fd30b98efa9e6d287ed69759bd3db7de
Mautaliev Saidamir <mautalievsaidamir@gmail.com>
2026-04-23 21:28:23 +0500

    Task0fuck

---
+++ Tyuiu.MautalievSI.Sprint5.Task0.V30.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.MautalievSI.Sprint5.Task0.V30.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
