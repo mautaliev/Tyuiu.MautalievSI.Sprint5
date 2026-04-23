e720bfb82a8f955ac4096b18e010bd85b5663860
Mautaliev Saidamir <mautalievsaidamir@gmail.com>
2026-04-23 23:09:45 +0500

    Task3

---
+++ Tyuiu.MautalievSI.Sprint5.Task3.V24.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.MautalievSI.Sprint5.Task3.V24.Lib
+{
+    public class DataService : ISprint5Task3V24
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
+
+            // Вычисляем значение функции
+            double result = 6.1 * Math.Pow(x, 3) + 0.23 * Math.Pow(x, 2) + 1.04 * x;
+            result = Math.Round(result, 3); // Округляем до 3 знаков
+
+            // Сохраняем результат в бинарный файл
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.Create)))
+            {
+                writer.Write(result);
+            }
+
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.MautalievSI.Sprint5.Task3.V24.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.MautalievSI.Sprint5.Task3.V24.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
